0f79c82e6df86d9aad330f31f244415e2bb8da84
ddulewicz99 <denis.dulewicz99@gmail.com>
2025-11-10 12:29:56 +0100

    Update GKiPO_KP_Zad1.py

--- GKiPO_KP_Zad1.py
+++ GKiPO_KP_Zad1.py
@@ -6,7 +6,6 @@ image = cv2.imread("Obraz.jpg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.imshow(image)
 plt.title("Oryginalny obraz")
-plt.show()
 
 height, width = image.shape[:2]
 new_width = width // 2
@@ -17,7 +16,6 @@ gray_image = cv2.cvtColor(resized_image, cv2.COLOR_RGB2GRAY)
 
 plt.imshow(gray_image, cmap='gray')
 plt.title("Obraz w skali szarości (50%)")
-plt.show()
 
 rotated_image = cv2.rotate(gray_image, cv2.ROTATE_90_CLOCKWISE)
 
@@ -28,6 +26,3 @@ plt.show()
 print("Rozmiar obrazu:", rotated_image.shape)
 print("Macierz obrazu (fragment 10x10):")
 print(rotated_image[:10, :10])
-
-cv2.imwrite("Obraz_koncowy.jpg", rotated_image)
-print("Wynikowy obraz zapisany jako 'Obraz_koncowy.jpg'")
